6a53c0b5b3d25b1be67a19d16c0b0079935ef926
veerasai18 <69525724+veerasai18@users.noreply.github.com>
2021-01-23 10:09:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+# pip install tweepy if you don't have tweepy installed
+import tweepy
+import pandas as pd
+import time
+
+#user credentials to authenticate API
+#you will get these credentilas when creating a developer account on Twitter
+consumer_key = "cdxBcqlqkQMrlBJSbQa6NNrIh"
+consumer_secret = "ZJf84rcKXCTDe2M2h3eZYjUKzHQ2z2biVa5Wm7PXUIvO6vzBOv"
+access_token = "912646830508228616-sUiZIEBiKUHaCyifjljdb0ytH33sCKS"
+access_token_secret = "pT9wrc4hJAKP8EPRQOUPstX0KoLJ7ZOOLt73CyVkApZci"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth,wait_on_rate_limit=True)
+
+#creating a list of tweets
+tweets = []
+
+#specify whose tweets you're scraping
+username = 'imVkohli'
+#number of tweets you need
+tweets_num = 10
+
+#defining a converting function which takes username and count as parameters
+def tweets_to_csv(username,tweets_num):
+
+        #getting the individual tweets
+        for tweet in api.user_timeline(id=username, count=tweets_num):
+
+            #adding tweets to the list
+            tweets.append((tweet.created_at,tweet.id,tweet.text))
+
+            #creating a dataframe in pandas and defining column names
+            df = pd.DataFrame(tweets,columns=['Date', 'Tweet_ID', 'Message'])
+
+            #converting dataframe to CSV file
+            df.to_csv('scraped_tweets.csv')
+
+
+#calling the function to create the CSV file
+tweets_to_csv(username, tweets_num)
